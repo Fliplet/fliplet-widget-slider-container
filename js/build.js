4741e35fcf7abe749110449ff52759c3ac1aae0c
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-10-10 10:44:11 +0200

    debugging

--- js/build.js
+++ js/build.js
@@ -334,34 +334,37 @@ Fliplet.Widget.instance({
       slider.swiper = swiper;
 
       swiper.on("slideChange", async function () {
-        let slideObject = this;
+        swiper.allowSlideNext = false;
+        swiper.allowSlidePrev = false;
+        
+        // let slideObject = this;
 
-        $sliderElement.find("video, audio").each(function () {
-          this.pause();
-        });
+        // $sliderElement.find("video, audio").each(function () {
+        //   this.pause();
+        // });
 
-        let currentSlide = slides[swiper.realIndex];
-        let $activeSlide = currentSlide.$el;
-        let $formElement = $activeSlide.find(
-          '[data-widget-package="com.fliplet.form-builder"]'
-        );
-        let formId = $formElement ? $formElement.data("id") : '';
+        // let currentSlide = slides[swiper.realIndex];
+        // let $activeSlide = currentSlide.$el;
+        // let $formElement = $activeSlide.find(
+        //   '[data-widget-package="com.fliplet.form-builder"]'
+        // );
+        // let formId = $formElement ? $formElement.data("id") : '';
 
-        if (currentSlide && currentSlide.fields.requiredForm && !submittedForms.includes(formId)) {
-          slideObject.allowSlidePrev =
-            !currentSlide.fields.requiredFormBackwardNavigation;
-          slideObject.allowSlideNext =
-            !currentSlide.fields.requiredFormForwardNavigation;
-        } else {
-          slideObject.allowSlidePrev = true;
-          slideObject.allowSlideNext = true;
-        }
+        // if (currentSlide && currentSlide.fields.requiredForm && !submittedForms.includes(formId)) {
+        //   slideObject.allowSlidePrev =
+        //     !currentSlide.fields.requiredFormBackwardNavigation;
+        //   slideObject.allowSlideNext =
+        //     !currentSlide.fields.requiredFormForwardNavigation;
+        // } else {
+        //   slideObject.allowSlidePrev = true;
+        //   slideObject.allowSlideNext = true;
+        // }
 
-        swiper.updateAutoHeight(500);
+        // swiper.updateAutoHeight(500);
 
-        if (!interactMode) {
-          scrollToTopOfSlide();
-        }
+        // if (!interactMode) {
+        //   scrollToTopOfSlide();
+        // }
       });
 
       manageSliderActions();
